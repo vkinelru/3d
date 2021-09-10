227e029b6e37a1f9e2784518bf8531dcb36d901b
vkinelru <vkinelru@gmail.com>
2021-09-10 19:41:07 +0400

    sauna

---
+++ sauna/data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Комната отдыха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.9568916363520863,
+        "pitch": 0.0292911366614117,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0499726016100386,
+          "pitch": 0.26893808559917076,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 0.09646702890209014,
+          "pitch": 0.27201160893910803,
+          "rotation": 2.356194490192345,
+          "target": "3---"
+        },
+        {
+          "yaw": 0.33521479717867564,
+          "pitch": 0.6110397836743982,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": 2.5053259749803907,
+          "pitch": 0.42889424102725826,
+          "rotation": 1.5707963267948966,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Комната отдыха. Центр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.640119854006535,
+        "pitch": -0.000993456014766636,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7813347072226904,
+          "pitch": 0.17575358444734235,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 2.7864501651099385,
+          "pitch": 0.41260782140595786,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2----",
+      "name": "Комната отдыха малая. Центр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0788199678835753,
+        "pitch": 0.4394674274017536,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4380545104776417,
+          "pitch": 0.6478711925042404,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -2.637111197007904,
+          "pitch": 0.16156028940480383,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 0.09345719018737242,
+          "pitch": 0.6485352960862514,
+          "rotation": 0,
+          "target": "3---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "Комната отдыха малая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6199954178880223,
+        "pitch": 0.3052373784201201,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.103236083025063,
+          "pitch": 0.8512324474352191,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": -2.918141759055297,
+          "pitch": 0.48154305992751745,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -3.002543883566945,
+          "pitch": 0.14349606140913806,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Душевая с бассейном",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.9672361113916725,
+        "pitch": 0.07878677202886308,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1730175128465774,
+          "pitch": 0.3710371385741187,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": 2.5885581897696532,
+          "pitch": 0.059773358079901584,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Сауна",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1254877815718505,
+        "pitch": 0.42315353438782033,
+        "fov": 1.4970014613970015
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.767704858466903,
+          "pitch": 0.4017076606106418,
+          "rotation": 1.5707963267948966,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Сауна. ВКинель.ру",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
