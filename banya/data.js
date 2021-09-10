6c410095f5552a63859e2fc07d7f97401baf9742
vkinelru <vkinelru@gmail.com>
2021-09-10 19:07:38 +0400

    banya

---
+++ banya/data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-----",
+      "name": "Вид с площадки коттеджа Ливерпуль",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0978278209529133,
+        "pitch": -0.008408771131094284,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.739086650409119,
+          "pitch": 0.01138639726126911,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": 2.344041756917756,
+          "pitch": -0.12734890717552005,
+          "rotation": 1.5707963267948966,
+          "target": "1-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-----",
+      "name": "Вид с крыльца коттеджа Ливерпуль",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.454018031641363,
+        "pitch": 0.018809064867697955,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8787082939260484,
+          "pitch": 0.40750602137495306,
+          "rotation": 4.71238898038469,
+          "target": "0-----"
+        },
+        {
+          "yaw": 1.326757560137363,
+          "pitch": 0.18532444329478182,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Предбанник и санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8969013686249312,
+        "pitch": 0.0015160234795246197,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6729148490706347,
+          "pitch": 0.3167892020915737,
+          "rotation": 0,
+          "target": "0-----"
+        },
+        {
+          "yaw": 0.40953109649152,
+          "pitch": 0.22852308684866074,
+          "rotation": 4.71238898038469,
+          "target": "3---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---2",
+      "name": "Комната отдыха 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.9307053287533193,
+        "pitch": 0.011935619563001154,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.439584327837526,
+          "pitch": 0.012524139727601025,
+          "rotation": 1.5707963267948966,
+          "target": "2---"
+        },
+        {
+          "yaw": 2.970101035529062,
+          "pitch": 0.034322577076249416,
+          "rotation": 4.71238898038469,
+          "target": "4---1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---1--",
+      "name": "Комната отдыха 1. У окна",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.066994445279395,
+        "pitch": 0.027530550433125356,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.094224826518178,
+          "pitch": -0.00003946431280432705,
+          "rotation": 10.995574287564278,
+          "target": "3---2"
+        },
+        {
+          "yaw": -2.665853967271456,
+          "pitch": 0.05932792257011599,
+          "rotation": 14.137166941154074,
+          "target": "7-"
+        },
+        {
+          "yaw": -3.055061425949404,
+          "pitch": 0.785296646538912,
+          "rotation": 0,
+          "target": "6---1-"
+        },
+        {
+          "yaw": -2.9606537302596205,
+          "pitch": 0.3058890309903095,
+          "rotation": 3.141592653589793,
+          "target": "5---1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---1--",
+      "name": "Комната отдыха 1. У дивана",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.7641784220129217,
+        "pitch": 0.009203521496583988,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.043243352567993,
+          "pitch": 0.5316552858782249,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        },
+        {
+          "yaw": 2.086049822890418,
+          "pitch": 0.0978926512756555,
+          "rotation": 1.5707963267948966,
+          "target": "8-"
+        },
+        {
+          "yaw": 3.126382486040166,
+          "pitch": 0.4073207330623685,
+          "rotation": 0,
+          "target": "6---1-"
+        },
+        {
+          "yaw": -3.0649128915553856,
+          "pitch": 0.10166596987468424,
+          "rotation": 0,
+          "target": "4---1--"
+        },
+        {
+          "yaw": -2.5310281867160054,
+          "pitch": -0.02223208149856859,
+          "rotation": 1.5707963267948966,
+          "target": "3---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---1-",
+      "name": "Комната отдыха 1. Центр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.13544753297127,
+        "pitch": 0.024521517816125993,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7866821236214587,
+          "pitch": 0.312305781065616,
+          "rotation": 1.5707963267948966,
+          "target": "3---2"
+        },
+        {
+          "yaw": 1.587611714279519,
+          "pitch": 0.1218571843150329,
+          "rotation": 4.71238898038469,
+          "target": "2---"
+        },
+        {
+          "yaw": 1.6149804616253025,
+          "pitch": -0.046933863933148245,
+          "rotation": 0,
+          "target": "0-----"
+        },
+        {
+          "yaw": 0.013439718815908108,
+          "pitch": 0.5041346421199027,
+          "rotation": 0,
+          "target": "4---1--"
+        },
+        {
+          "yaw": -2.42653657908712,
+          "pitch": 0.12397035321201422,
+          "rotation": 1.5707963267948966,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.8069195575467365,
+          "pitch": 0.208968278826962,
+          "rotation": 0,
+          "target": "5---1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Мойка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.2617101383383993,
+        "pitch": 0.08326041276107965,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0798476508185457,
+          "pitch": -0.14534107418443654,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -2.8880086729649186,
+          "pitch": 0.31818454428768916,
+          "rotation": 4.71238898038469,
+          "target": "6---1-"
+        },
+        {
+          "yaw": -2.6460154489449828,
+          "pitch": 0.31322474769135056,
+          "rotation": 1.5707963267948966,
+          "target": "5---1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Парилка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.260652198843422,
+        "pitch": 0.5049501983093965,
+        "fov": 1.5364884221594795
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2062879234357045,
+          "pitch": 0.45481016406476016,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Русская баня. ВКинель.ру",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
