25a29315c017b18bdff974a1fd941ac53c187921
vkinelru <vkinelru@gmail.com>
2021-10-16 17:01:50 +0400

    2021-10-16_17-01-49

--- push2github.sh
+++ push2github.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 set -x;
-time (git add --verbose --all; git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --force --verbose;)
+time (echo git pull --verbose; git add --verbose --all; git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --verbose;)
