f76bbd116f99a540aa8a36b589d4c3dac2c52275
vkinelru <vkinelru@gmail.com>
2022-07-07 17:47:13 +0400

    2022-07-07_17-47-13

--- index/vkinels.js
+++ index/vkinels.js
@@ -3,29 +3,6 @@ yametrika_id = 51096746;
 
 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(yametrika_id, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true });
 
-
-(function(m, e, t, r, i, k, a) {
-    m[i] = m[i] || function() {
-        (m[i].a = m[i].a || []).push(arguments)
-    }
-    ;
-    m[i].l = 1 * new Date();
-    k = e.createElement(t),
- a = e.getElementsByTagName(t)[0],
- k.async = 1,
- k.src = r,
- a.parentNode.insertBefore(k, a)
-}
-)(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-ym(yametrika_id, "init", {
-    clickmap: true,
-   trackLinks: true,
-   accurateTrackBounce: true,
-   webvisor: true,
-   trackHash: true,
-   ecommerce: "dataLayer"
-});
-
 function all_inputs_handler(callback_fun) {
     var textFields = document.querySelectorAll('input');
     var index = 0;
