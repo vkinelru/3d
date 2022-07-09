db22730c3904946fd3594dc097508a4efa16cd43
vkinelru <vkinelru@gmail.com>
2022-07-09 23:00:17 +0400

    2022-07-09_23-00-17

--- index/vkinels.js
+++ index/vkinels.js
@@ -150,6 +150,73 @@ if (!document.getElementsByClassName)
     Element.prototype.getElementsByClassName = document.getElementsByClassName;
 }
 
+
+// Шаги алгоритма ECMA-262, 5-е издание, 15.4.4.14
+// Ссылка (en): http://es5.github.io/#x15.4.4.14
+// Ссылка (ru): http://es5.javascript.ru/x15.4.html#x15.4.4.14
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(searchElement, fromIndex) {
+        var k;
+
+        // 1. Положим O равным результату вызова ToObject с передачей ему
+        //    значения this в качестве аргумента.
+        if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+        }
+
+        var O = Object(this);
+
+        // 2. Положим lenValue равным результату вызова внутреннего метода Get
+        //    объекта O с аргументом "length".
+        // 3. Положим len равным ToUint32(lenValue).
+        var len = O.length >>> 0;
+
+        // 4. Если len равен 0, вернём -1.
+        if (len === 0) {
+            return -1;
+        }
+
+        // 5. Если был передан аргумент fromIndex, положим n равным
+        //    ToInteger(fromIndex); иначе положим n равным 0.
+        var n = +fromIndex || 0;
+
+        if (Math.abs(n) === Infinity) {
+            n = 0;
+        }
+
+        // 6. Если n >= len, вернём -1.
+        if (n >= len) {
+            return -1;
+        }
+
+        // 7. Если n >= 0, положим k равным n.
+        // 8. Иначе, n<0, положим k равным len - abs(n).
+        //    Если k меньше нуля 0, положим k равным 0.
+        k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+        // 9. Пока k < len, будем повторять
+        while (k < len) {
+            // a. Положим Pk равным ToString(k).
+            //   Это неявное преобразование для левостороннего операнда в операторе in
+            // b. Положим kPresent равным результату вызова внутреннего метода
+            //    HasProperty объекта O с аргументом Pk.
+            //   Этот шаг может быть объединён с шагом c
+            // c. Если kPresent равен true, выполним
+            //    i.  Положим elementK равным результату вызова внутреннего метода Get
+            //        объекта O с аргументом ToString(k).
+            //   ii.  Положим same равным результату применения
+            //        Алгоритма строгого сравнения на равенство между
+            //        searchElement и elementK.
+            //  iii.  Если same равен true, вернём k.
+            if (k in O && O[k] === searchElement) {
+                return k;
+            }
+            k++;
+        }
+        return -1;
+    };
+}
+
 function hide_element_by_class(class_name)
 {
     var all = document.getElementsByClassName(class_name);
