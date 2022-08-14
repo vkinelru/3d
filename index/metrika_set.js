40af556acc4d652b3a5d20fe0ca5d186f1b35828
vkinelru <vkinelru@gmail.com>
2022-08-14 14:28:51 +0400

    2022-08-14_14-28-51

--- index/metrika_set.js
+++ index/metrika_set.js
@@ -266,6 +266,7 @@ function init_after_page_loaded()
     metrika_counter_id = get_params['metrika_counter_id'];
     window.metrika_counter_id = metrika_counter_id;
     mylog('metrika_counter_id='+metrika_counter_id);
+    get_params['USERID'] = get_params['user_id'];
     get_params['UserID'] = get_params['user_id'];
     get_params['userID'] = get_params['user_id'];
     get_params['userid'] = get_params['user_id'];
